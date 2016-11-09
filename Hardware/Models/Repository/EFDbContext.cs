19655dcbc98adebe47c82bdb4be781d27154d00e
cleverjew <solembum@me.com>
2016-11-10 00:14:53 +0200

    Model

---
+++ Hardware/Models/Repository/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace Hardware.Models.Repository
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Hardware> Products { get; set; }
+    }
+}
---
+++ Hardware/Models/Repository/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hardware.Models.Repository
+{
+    public class Repository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Hardware> Games
+        {
+            get { return context.Products; }
+        }
+    }
+}
